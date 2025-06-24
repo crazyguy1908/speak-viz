71bc45a2795936e985483788df225e58e07a3fa9
Vedansh Pilani <71386399+crazyguy1908@users.noreply.github.com>
2025-06-24 21:11:54 +0530

    Revert "Fixed the shit"
    This reverts commit 2e82f832ac6ee2314be3cfc19a19dc9f60fb142a.

--- frontend/speakviz-frontend/src/app/Recorder.js
+++ frontend/speakviz-frontend/src/app/Recorder.js
@@ -16,50 +16,10 @@ function Recorder() {
     if (!stream) {
       return null;
     }
-<<<<<<< Updated upstream
     return (
       <video className="video-preview-player" ref={videoRef} autoPlay muted />
     );
   };
-=======
-    return <video className="video-preview-player" ref={videoRef} autoPlay muted />;
-};
-    
-
-    const handleDownload = (blobUrl) => {
-        const fileName = `speakviz-recording-${Date.now()}.webm`;
-
-        const link = document.createElement('a');
-        link.href = blobUrl;
-        link.download = fileName;
-        document.body.appendChild(link);
-    }
-
-    return (
-        <>
-            <div>
-                <ReactMediaRecorder
-                video
-                onStop={handleDownload}
-                render={({ status, startRecording, stopRecording, mediaBlobUrl, previewStream }) => (
-                    <div className="recorder-container">
-                        <h1>{status}</h1>
-                        <div className="controls">
-                            <button className="start-button" onClick={startRecording} disabled={status === "recording" ? true : false}>Start Recording</button>
-                            <button className="stop-button" onClick={stopRecording} disabled={status == "idle" ? true : false}>Stop Recording</button>
-                        </div>
-                        {status === 'stopped' && mediaBlobUrl ? (
-                            <video className="video-player" src={mediaBlobUrl} controls autoPlay loop />
-                        ) : (
-                            <VideoPreview stream={previewStream} />
-                        )}
-                    </div>
-                )}
-                />
-            </div>
-        </>
-    )
->>>>>>> Stashed changes
 
   const handleDownload = (blobUrl) => {
     const fileName = `speakviz-recording-${Date.now()}.webm`;
