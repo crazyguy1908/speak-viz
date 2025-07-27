e3a313755459fd0fa75dabc4ea779dfcc01b04ea
Vedansh Pilani <71386399+crazyguy1908@users.noreply.github.com>
2025-07-28 00:43:58 +0530

    nav bar changes

--- frontend/speakviz-frontend/src/app/components/navbar.js
+++ frontend/speakviz-frontend/src/app/components/navbar.js
@@ -1,58 +1,23 @@
 import * as React from "react"
 import Link from "next/link"
-
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
-
 import './navbar.css';
 
 export default function Navbar() {
   return (
     <div className="svz-navbar-root">
-      <NavigationMenu className="svz-navbar-menu">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            {/* Only use our custom class, not shadcn's navigationMenuTriggerStyle */}
-            <NavigationMenuTrigger className="svz-navbar-trigger">
-              Menu
-            </NavigationMenuTrigger>
-            <NavigationMenuContent className="svz-navbar-dropdown">
-              <div className="svz-navbar-dropdown-list">
-                <NavigationMenuLink asChild>
-                  <Link 
-                    href="/" 
-                    className="svz-navbar-dropdown-link"
-                  >
-                    Home
-                  </Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link 
-                    href="/playback" 
-                    className="svz-navbar-dropdown-link"
-                  >
-                    Playback
-                  </Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link 
-                    href="/recorder" 
-                    className="svz-navbar-dropdown-link"
-                  >
-                    Recorder
-                  </Link>
-                </NavigationMenuLink>
-              </div>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <nav className="svz-navbar-menu">
+        <div className="svz-navbar-links">
+          <Link href="/homepage" className="svz-navbar-link">
+            Home
+          </Link>
+          <Link href="/recordings" className="svz-navbar-link">
+            Playback
+          </Link>
+          <Link href="/recorder" className="svz-navbar-link">
+            Recorder
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 }
