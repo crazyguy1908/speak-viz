90a6e7c6a803887b1b40e5c999888f2f1c5426c4
Vedansh Pilani <71386399+crazyguy1908@users.noreply.github.com>
2025-08-01 15:17:19 +0530

    env variables added

--- frontend/speakviz-frontend/src/supabaseClient.js
+++ frontend/speakviz-frontend/src/supabaseClient.js
@@ -1,3 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
-const supabase = createClient('https://hdnudubhrjynjdioaqdc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkbnVkdWJocmp5bmpkaW9hcWRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MjIwNjYsImV4cCI6MjA2NzI5ODA2Nn0.O1AxSTfFhi1PPchyMRkrYS045fzQJKE46j0GQhOLA6c')
-export { supabase };
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
